9bb81efe686fe91a86e5b6a3d318751f53b33eef
althaf <shaikalthaf1984@gmail.com>
2025-09-29 20:52:02 +0530

    roboshop.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USER_ID=$(id -u)
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -8,24 +8,28 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+START_TIME=$(date +%s)
 
-    mkdir -p $LOGS_FOLDER
-    echo "script started excuted at: $(date)"
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
-if [ $USER_ID -ne 0 ]; then
-    echo "ERROR :: Please run this script with root privelege"
-    exit 1
-fi 
+check_root(){
+    if [ $USERID -ne 0 ]; then
+        echo "ERROR:: Please run this script with root privelege"
+        exit 1 # failure is other than 0
+    fi
+}
 
-VALIDATE(){
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "Installing $2...$R failure $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "Installing $2...$G success $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
-   }
+}
  
  cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding Mongo repo"
