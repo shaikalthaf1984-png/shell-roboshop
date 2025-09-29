0f761cf5b12bcc82a221c1b654fc08d7372588fd
althaf <shaikalthaf1984@gmail.com>
2025-09-29 17:13:37 +0530

    roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-010939a0bddaba727 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-if [ $instance != "frontend" ] then
+if [ $instance != "frontend" ]; then
     IP=$(aws ec2 describe-instances --instance-ids i-0d23a660282610957 --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
 else
     IP=$(aws ec2 describe-instances --instance-ids i-0d23a660282610957 --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
