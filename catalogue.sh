5dabd0c4ec60ec4ab162fdd3fb0741364cf8b9e9
althaf <shaikalthaf1984@gmail.com>
2025-09-30 17:05:30 +0530

    catalogue.sh

--- catalogue.sh
+++ catalogue.sh
@@ -8,7 +8,7 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-SCRIPT_DIR=$PwD
+SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.althaf84.org
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
