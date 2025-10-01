8743ed74403539e1473583a30f9166129a8cbf24
althaf <shaikalthaf1984@gmail.com>
2025-10-01 17:59:06 +0530

    frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.althaf84.org
+MONGODB_HOST=mongodb.althaf84.org.fun
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 mkdir -p $LOGS_FOLDER
@@ -32,23 +32,21 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
 dnf module disable nginx -y &>>$LOG_FILE
 dnf module enable nginx:1.24 -y &>>$LOG_FILE
 dnf install nginx -y &>>$LOG_FILE
-VALIDATE $? "Installing nginx"
+VALIDATE $? "Installing Nginx"
 
-systemctl enable nginx &>>$LOG_FILE
+systemctl enable nginx  &>>$LOG_FILE
 systemctl start nginx 
-VALIDATE $? "Starting nginx"
+VALIDATE $? "Starting Nginx"
 
-rm -rf /usr/share/nginx/html/*
-
+rm -rf /usr/share/nginx/html/* 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Downloading frontend"
 
 rm -rf /etc/nginx/nginx.conf
-
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
-VALIDATE $? "copying nginx.conf"
+VALIDATE $? "Copying nginx.conf"
 
 systemctl restart nginx 
-VALIDATE $? "Restarting nginx"
+VALIDATE $? "Restarting Nginx"
