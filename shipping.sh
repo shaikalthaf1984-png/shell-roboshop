9c8489041ef49d5da024f353de9eed4f39703adb
althaf <shaikalthaf1984@gmail.com>
2025-10-02 13:40:50 +0530

    shellroboshop.sh

--- shipping.sh
+++ shipping.sh
@@ -30,10 +30,9 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     fi
 }
 
-    dnf install maven -y
-    VALIDATE $? "Installing maven"
+dnf install maven -y &>>$LOG_FILE
 
-    id roboshop &>>$LOG_FILE
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "Creating system user"
@@ -41,9 +40,8 @@ else
     echo -e "User already exist ... $Y SKIPPING $N"
 fi
 
-
-   mkdir -p /app
-    VALIDATE $? "Creating app directory"
+mkdir -p /app
+VALIDATE $? "Creating app directory"
 
 curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading shipping application"
@@ -56,26 +54,23 @@ VALIDATE $? "Removing existing code"
 
 unzip /tmp/shipping.zip &>>$LOG_FILE
 VALIDATE $? "unzip shipping"
- 
-mvn clean package &>>$LOG_FILE
+
+mvn clean package  &>>$LOG_FILE
 mv target/shipping-1.0.jar shipping.jar 
 
 cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
-
 systemctl daemon-reload
-systemctl enable shipping &>>$LOG_FILE
-systemctl start shipping
+systemctl enable shipping  &>>$LOG_FILE
 
-dnf install mysql -y &>>$LOG_FILE
+dnf install mysql -y  &>>$LOG_FILE
 
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]; then
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
 else
-echo -e "Shipping data is already loadded....$Y SKIPPING $N"
+    echo -e "Shipping data is already loaded ... $Y SKIPPING $N"
 fi
 
 systemctl restart shipping
-
