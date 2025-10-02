3da9249592bfe2850ed11560e304debb5a388205
althaf <shaikalthaf1984@gmail.com>
2025-10-02 16:30:50 +0530

    20-script.sh

--- 20-script.sh
+++ 20-script.sh
@@ -6,4 +6,4 @@ echo "My Country is: $NAME"
 echo "PID of 20-script-1 is: $$"
 #sh 21-script-2.sh
 
-source ./21-script-2.sh
+source ./21-script-2.shs
--- 21-script-2.sh
+++ 21-script-2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-echo "My country is: $NAME
+echo "My country is: $NAME"
 echo "PID of script-2 is: $$"
