705e2e28feb4cb79b26f29682a8e85ca2c8932f5
althaf <shaikalthaf1984@gmail.com>
2025-10-02 13:22:48 +0530

    shellroboshop.sh

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -10,9 +10,8 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
-SCRIPT_DIR=$PWD
-
 mkdir -p $LOGS_FOLDER
+SCRIPT_DIR=$PWD
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
@@ -29,26 +28,18 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     fi
 }
 
-    cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
-    VALIDATE $? "Adding rabbitmq repo"
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
+VALIDATE $? "Adding RabbitMQ repo"
+dnf install rabbitmq-server -y &>>$LOG_FILE
+VALIDATE $? "Installing RabbitMQ Server"
+systemctl enable rabbitmq-server &>>$LOG_FILE
+VALIDATE $? "Enabling RabbitMQ Server"
+systemctl start rabbitmq-server &>>$LOG_FILE
+VALIDATE $? "Starting RabbitMQ"
+rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
+VALIDATE $? "Setting up permissions"
 
-    dnf install rabbitmq-server -y &>>$LOG_FILE
-    VALIDATE $? "Installling rabbitmq server"
-
-    systemctl enable rabbitmq-server &>>$LOG_FILE
-    VALIDATE $? "Enabling rabbitmq server"
-
-    systemctl start rabbitmq-server &>>$LOG_FILE
-    VALIDATE $? "starting rabbitmq server"
-
-    rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
-
-    rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
-    VALIDATE $? "Setting up permissions"
-
-    END_TIME=$(date +%s)
-    TOTAL_TIME=$(( $END_TIME-$START_TIME ))
-    echo -e "Script executed in: $Y $TOTAL_TIME seconds $N"
-
-
- 
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
